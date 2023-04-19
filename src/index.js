b787e814ca5c8e4efc37fe83501a0e4985b7b86d
Shane Jampole <shane.jampole.98@gmail.com>
2023-04-19 17:42:36 -0400

    committing labwork

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const f = document.querySelector(`#create-task-form`)
+  f.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let answer = e.target.querySelector(`#new-task-description`).value
+    document.querySelector(`#tasks`).innerHTML = answer
+    e.target.reset()
+  })
 });
